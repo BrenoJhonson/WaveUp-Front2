bf8efbb122197e2acfbadbae4bedf82c53d95046
BrenoJhonson <carlosbrenops01@gmail.com>
2024-12-03 02:41:19 -0300

    Mudança no caminho das pastas

--- src/components/ThumbNail/Style.jsx
+++
@@ -1,6 +0,0 @@
-import Styled from 'styled-components'
-
-
-export const CardImage = Styled.img`
-    width: 15%;
-`;
--- src/constants/urls.js
+++
@@ -1,2 +0,0 @@
-export const BASE_URL =
-  "https://mqjnto3qw2.execute-api.us-east-1.amazonaws.com/default/";
