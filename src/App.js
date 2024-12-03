c10fd2979a243e2628e42d8c0918d95df49cde1a
BrenoJhonson <carlosbrenops01@gmail.com>
2024-12-03 02:22:30 -0300

    correção app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter } from "react-router-dom";
 import Router from "./routes/Router";
 import GlobalStyle from "./styles/Styles";
@@ -16,22 +15,3 @@ function App() {
 }
 
 export default App;
-=======
-import { BrowserRouter } from "react-router-dom";
-import Router from "./routes/Router";
-import GlobalStyle from "./styles/Styles";
-
-
-function App() {
-  return (
-    <>
-      <GlobalStyle />
-      <BrowserRouter basename="/spoticry-front">
-        <Router />
-      </BrowserRouter>
-    </>
-  );
-}
-
-export default App;
->>>>>>> c4298f330b0def9f81e1aebbd0d9aa623ca0a582
